4f2d53c6502d199b7215285d2f97bf91e93d4e66
sasholilov <alex_xld@abv.bg>
2024-04-15 21:58:20 +0300

    move add supplier into separate component

---
+++ src/features/suppliers/AddSupplier.jsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import TableData from "../../ui/TableData";
+import SaveIcon from "../../ui/SaveIcon";
+import Button from "../../ui/Button";
+import InputText from "../../ui/InputText";
+import { useState, useRef } from "react";
+import { useAddSupplier } from "./useAddSupplier";
+import { toast } from "react-hot-toast";
+
+const StyledForm = styled.form`
+  grid-column: 1 / 7;
+  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(7, minmax(180px, 1fr));
+  text-align: center;
+`;
+
+function AddSupplier() {
+  const ref = useRef();
+  const { isAdding, addSupplier } = useAddSupplier();
+  const [company_name, setCompany_name] = useState({});
+  const [vat, setVat] = useState({});
+  const [address, setAddress] = useState({});
+  const [telephone, setTelephone] = useState({});
+  const [iban, setIban] = useState({});
+  const [country, setCountry] = useState({});
+
+  function handleSaveSupplier(e) {
+    e.preventDefault();
+    const suplierObj = {
+      company_name,
+      vat,
+      address,
+      telephone,
+      iban,
+      country,
+    };
+
+    if (!company_name || !vat || !address || !telephone || !iban || !country) {
+      toast.error("Please fill all fields");
+      return;
+    }
+    if (isAdding) return <p>Loading....</p>;
+    addSupplier({ ...suplierObj }, { onSuccess: ref.current.reset() });
+    setAddress("");
+    setCompany_name("");
+    setVat("");
+    setIban("");
+    setTelephone("");
+    setCountry("");
+  }
+
+  if (isAdding) return <p>Loading....</p>;
+
+  return (
+    <StyledForm ref={ref}>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="Company Name"
+          onChange={(e) => setCompany_name(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="VAT"
+          onChange={(e) => setVat(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="Country"
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="Address"
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="Telephone"
+          onChange={(e) => setTelephone(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <InputText
+          type="text"
+          placeholder="IBAN"
+          onChange={(e) => setIban(e.target.value)}
+        />
+      </TableData>
+      <TableData>
+        <Button type="save" onClick={(e) => handleSaveSupplier(e)}>
+          <SaveIcon />
+        </Button>
+      </TableData>
+    </StyledForm>
+  );
+}
+
+export default AddSupplier;
--- src/features/suppliers/Suppliers.jsx
+++ src/features/suppliers/Suppliers.jsx
@@ -1,20 +1,16 @@
 import Table from "../../ui/Table";
 import TableHeader from "../../ui/TableHeader";
-import TableData from "../../ui/TableData";
 import Title from "../../ui/Title";
 import styled from "styled-components";
 import { useSuppliers } from "./useSuppliers";
 import Button from "../../ui/Button";
-import SaveIcon from "../../ui/SaveIcon";
 import DeleteIcon from "../../ui/DeleteIcon";
 import EditIcon from "../../ui/EditIcon";
-import InputText from "../../ui/InputText";
-import { useAddSupplier } from "./useAddSupplier";
-import { useState, useRef } from "react";
-import { toast } from "react-hot-toast";
+import { useState } from "react";
 import TableDataRow from "../../ui/TableDataRow";
 import { useDeleteSupplier } from "./useDeleteSupplier";
 import EditSupplier from "./EditSupplier";
+import AddSupplier from "./AddSupplier";
 
 const StyledHeaderBar = styled.div`
   display: flex;
@@ -24,55 +20,14 @@ const StyledHeaderBar = styled.div`
   margin-bottom: 10px;
 `;
 
-const StyledForm = styled.form`
-  grid-column: 1 / 7;
-  align-items: center;
-  display: grid;
-  grid-template-columns: repeat(7, minmax(120px, 1fr));
-  text-align: center;
-`;
-
 function Suppliers() {
-  const ref = useRef();
   const { isLoading, suppliers } = useSuppliers();
   const { isDeleting, deleteSupp } = useDeleteSupplier();
   const [addMode, setAddMode] = useState(false);
   const [editMode, setEditMode] = useState(false);
-  const [company_name, setCompany_name] = useState("");
-  const [vat, setVat] = useState("");
-  const [address, setAddress] = useState("");
-  const [telephone, setTelephone] = useState("");
-  const [iban, setIban] = useState("");
-  const [country, setCountry] = useState("");
   const [objectToEdit, setObjectToEdit] = useState({});
-  const { isAdding, addSupplier } = useAddSupplier();
   const addModeButton = addMode === true ? "close" : "add";
   if (isLoading || isDeleting) return <p>Loading...</p>;
-
-  function handleSaveSupplier(e) {
-    e.preventDefault();
-    const suplierObj = {
-      company_name,
-      vat,
-      address,
-      telephone,
-      iban,
-      country,
-    };
-
-    if (!company_name || !vat || !address || !telephone || !iban || !country) {
-      toast.error("Please fill all fields");
-      return;
-    }
-    if (isAdding) return <p>Loading....</p>;
-    addSupplier({ ...suplierObj }, { onSuccess: ref.current.reset() });
-    setAddress("");
-    setCompany_name("");
-    setVat("");
-    setIban("");
-    setTelephone("");
-    setCountry("");
-  }
 
   function handleEditSupplier(id) {
     const objectToEditToState = suppliers.find(
@@ -101,57 +56,7 @@ function Suppliers() {
           <p>Actions</p>
         </TableHeader>
 
-        {addMode && (
-          <StyledForm ref={ref}>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="Company Name"
-                onChange={(e) => setCompany_name(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="VAT"
-                onChange={(e) => setVat(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="Country"
-                onChange={(e) => setCountry(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="Address"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="Telephone"
-                onChange={(e) => setTelephone(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <InputText
-                type="text"
-                placeholder="IBAN"
-                onChange={(e) => setIban(e.target.value)}
-              />
-            </TableData>
-            <TableData>
-              <Button type="save" onClick={(e) => handleSaveSupplier(e)}>
-                <SaveIcon />
-              </Button>
-            </TableData>
-          </StyledForm>
-        )}
+        {addMode && <AddSupplier />}
         {editMode && (
           <EditSupplier
             objecToEdit={objectToEdit}
